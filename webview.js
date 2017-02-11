1a62253a22ac39448dc5cd3d2bb3e6b17e9fa875
Kan Fushihara <kan.fushihara@gmail.com>
2017-02-11 15:18:09 +0900

    Instagramのサイト修正に対応
    - 未読の反応をTOPページの内容だけでチェックできるようになった

--- webview.js
+++ webview.js
@@ -3,12 +3,6 @@ module.exports = (Franz, options) => {
         location.reload();
     }, 90000);
 
-    window.addEventListener('load', (ev) => {
-        clickNotify(); // open notify menu
-
-        setTimeout(checkNotify, 500);
-    });
-
     window.addEventListener('scroll', (ev) => {
         const now = document.querySelector('main section article header a time').getAttribute('datetime');
         localStorage.setItem("Franz.Instagram.latest", now);
@@ -27,37 +21,11 @@ module.exports = (Franz, options) => {
         return 0;
     }
 
-    function clickNotify() {
-        const elm = document.querySelector('.coreSpriteDesktopNavActivity');
-        const evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, elm);
-        elm.dispatchEvent(evt);
-    }
-
-    function checkNotify() {
-        if (document.querySelector('._mkiio._8kllr ._auspy time')) {
-            Franz.instagramNotify = document.querySelector('._mkiio._8kllr ._auspy time').getAttribute('datetime');
-            clickNotify(); // close notify menu
-
-            const elm = document.querySelector('.coreSpriteDesktopNavActivity');
-            elm.addEventListener('click', (ev) => {
-                localStorage.setItem("Franz.Instagram.notify", Franz.instagramNotify);
-            });
-        } else {
-            setTimeout(checkNotify, 500);
-        }
-    }
-
     function getNotify() {
-        const latest = localStorage.getItem("Franz.Instagram.notify");
-        console.log(Franz.instagramNotify);
-        if (!latest) {
-            localStorage.setItem("Franz.Instagram.notify", Franz.instagramNotify);
-        }
-        if (latest != Franz.instagramNotify) {
+        if (document.querySelectorAll('a._gx3bg > div')[2].classList.contains('_ea05m')) {
             return 1;
         }
-
+        
         return 0;
     }
 
